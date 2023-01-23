706e701769589db72612d5be384f11aa21e94f47
Danil <oldblacktree@gmail.com>
2023-01-24 02:39:56 +0300

    feat: add intro section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-
 import './Intro.css';
+import { BsFillPlayFill, BsPauseFill} from 'react-icons/bs';
+import {meal} from '../../constants'
+import { useRef, useState } from 'react';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const videoRef= useRef();
+  const [isVideoPlay, playVideo] = useState(false);
+  const handleToggleVideo = () => {
+    playVideo((prev) => !prev);
+    if(isVideoPlay) {
+      videoRef.current.pause()
+    } else {
+      videoRef.current.play()
+    }
+  }
+
+  return (
+    <div className='intro'>
+      <video 
+        src={meal} 
+        type="video/mp4"
+        ref={videoRef}
+        loop
+        controls={false}
+        muted>
+      </video>
+      <div className="intro__overlay flex-center">
+        <div className='intro__overlay-circle flex-center' onClick={handleToggleVideo}>
+          {isVideoPlay ? (< BsPauseFill color="#fff" fontSize={30}/>) : (< BsFillPlayFill color="#fff" fontSize={30}/>)}
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default Intro;
