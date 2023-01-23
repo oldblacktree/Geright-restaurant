6d151e07c8b655c539517996153d1987e1a37ae6
Danil <oldblacktree@gmail.com>
2023-01-24 00:22:05 +0300

    feat: add chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-
-import './Chef.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__chef section-padding section-bg ">
+    <div className="app__chef-img">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__chef-info">
+      <SubHeading title="Shef's word" />
+      <h2 className="p-headtext">What we believe in</h2>
+      <p className="p-opensans app__chef-info-quote">
+        <span>
+          <img src={images.quote} alt="quote" />
+        </span>{" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit
+        auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
+        Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
+        lectus eu. Congue iaculis integer curabitur semper sit nunc.
+      </p>
+      <div className="app__chef-sign">
+        <p className="app__chef-sign-name">Kevin Luo</p>
+        <p className="p-opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
